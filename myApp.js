54eb012530e47dcf862643a273d01dbcfb3b3651
Luis Pestana <theluispestana@gmail.com>
2020-07-25 02:46:15 +0000

    finished first several codecamp challenges

--- myApp.js
+++ myApp.js
@@ -9,19 +9,34 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
+//app.get('/', (req, res) => {
+  //res.send("Hello Express");
+//});
 
-
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
 /** 3) Serve an HTML file */
-
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+app.get('/json', (req, res) => {
+  const message = "Hello json"
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": message.toUpperCase()});
+  } else {
+    res.json({"message": message});
+  }
+});
 
 /** 6) Use the .env file to configure the app */
  
@@ -29,16 +44,26 @@ var app = express();
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({time: req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', (req, res) => {
+  res.json({echo: req.params.word})
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+const handleQuery = (req, res) => {
+  const { first, last } = req.query;
+  res.json({name: `${first} ${last}`});
+}
+app.route("/name").get(handleQuery).post(handleQuery);
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
